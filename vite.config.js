f828064b3a905496015c839dc250f2fc84c2399f
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-07-23 14:51:15 -0300

    fixing paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/mortgage-repayment-calculator-vite/", // Substitua pelo nome do seu repositório no GitHub
+    base: "./", // Substitua pelo nome do seu repositório no GitHub
     build: {
         outDir: "docs", // Define a pasta de saída como 'docs'
     },
